1d85de0156cb2a280bf142a38b6bda2c751d85d5
diegohs13 <diego.hsoliveira13@gmail.com>
2024-04-04 21:33:21 -0300

    primeiro-commit

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { NativeBaseProvider, View } from 'native-base';
+import AdicionarTarefa from "./components/AdicionarTarefa";
+import ListaTarefas from "./components/ListaTarefas";
+
+export default function App() {
+  const [tarefas, setTarefas] = useState<string[]>([]);
+
+  const adicionarTarefa = (novaTarefa: string) => {
+    setTarefas([...tarefas, novaTarefa]);
+  };
+
+  return (
+      <NativeBaseProvider>
+        <View style={{ flex: 1 }}>
+          <AdicionarTarefa onAdicionarTarefa={adicionarTarefa} />
+          <ListaTarefas tarefas={tarefas} />
+        </View>
+      </NativeBaseProvider>
+  );
+}
+
